6ca236a65fbf283f9bb977b23ec23e8829336877
ayushmanmanishankar <148039071+ayushmanmanishankar@users.noreply.github.com>
2024-03-20 16:19:01 +0530

    added more variables to url

--- ExoCtk.py
+++ ExoCtk.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup
 
 class ExoCtk:
     def __init__(self) -> None:
-        self.URL="https://exoctk.stsci.edu/generic?temperature={}&gravity={}&r_planet={}&r_star={}&condensation=local&metallicity=%2B0.0&c_o=0.35&haze=0001&cloud=0.00"
-    def generate_values(self,temp,g,r_planet,r_star):
-        response = requests.get(self.URL.format(temp,g,r_planet,r_star))
+        self.URL="https://exoctk.stsci.edu/generic?temperature={}&gravity={}&r_planet={}&r_star={}&condensation=local&metallicity=%2B{}&c_o={}&haze={}&cloud={}"
+    def generate_values(self,temp,g,r_planet,r_star,metallicity,c_o,haze,cloud):
+        response = requests.get(self.URL.format(temp,g,r_planet,r_star,metallicity,c_o,haze,cloud))
         try:
             soup = BeautifulSoup(response.content, 'html.parser')
             input_tag = soup.find_all("input")
--- main.py
+++ main.py
@@ -2,20 +2,31 @@ from ExoCtk import ExoCtk
 from Random import Random
 from FileHelper import FileHelper
 exo_ctk=ExoCtk()
-def generateData(temp,g,r_planet,r_star):
-    data_file=exo_ctk.generate_values(temp,g,r_planet,r_star)
+def generateData(temp,g,r_planet,r_star,metallicity,c_o,haze,cloud):
+    data_file=exo_ctk.generate_values(temp,g,r_planet,r_star,metallicity,c_o,haze,cloud)
     if data_file:
         folder_path="data_values"
         filename = '{}.txt'.format(Random().generate(10))
         FileHelper().insertOne(folder_path,filename,data_file)
 
 FileHelper().removeAll("data_values")
-temp=[400,500,600]
-g=[5,10,15]
-r_planet=[5,4,6]
-r_star=[3,5,6,100]
+temp=[400]
+for i in range(6,27):
+    temp.append(i*100)
+g=[5,10,20,50]
+r_planet=[5]
+r_star=[3]
+metallicity=["0.0","1.0","1.7","2.0","2.3"]
+c_o=["0.35","0.56","0.70","1.00"]
+haze=["0001","0010","0100","1100"]
+cloud=["0.35","0.56","0.70","1.00"]
+
 for i in temp:
     for j in g:
         for k in r_planet:
             for l in r_star:
-                generateData(i,j,k,l)
+                for m in metallicity:
+                    for n in c_o:
+                        for o in haze:
+                            for p in cloud:
+                                generateData(i,j,k,l,m,n,o,p)
