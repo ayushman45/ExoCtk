b52c0f4c9c2a6f719a9272bf9f3df49fb488b1be
ayushmanmanishankar <148039071+ayushmanmanishankar@users.noreply.github.com>
2024-03-20 17:39:31 +0530

    beautified

--- ExoCtk.py
+++ ExoCtk.py
@@ -1,18 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 class ExoCtk:
     def __init__(self) -> None:
-        self.URL="https://exoctk.stsci.edu/generic?temperature={}&gravity={}&r_planet={}&r_star={}&condensation=local&metallicity=%2B{}&c_o={}&haze={}&cloud={}"
-    def generate_values(self,temp,g,r_planet,r_star,metallicity,c_o,haze,cloud):
-        response = requests.get(self.URL.format(temp,g,r_planet,r_star,metallicity,c_o,haze,cloud))
+        self.URL = "https://exoctk.stsci.edu/generic?temperature={}&gravity={}&r_planet={}&r_star={}&condensation=local&metallicity=%2B{}&c_o={}&haze={}&cloud={}"
+
+    def generate_values(self, temp, g, r_planet, r_star, metallicity, c_o, haze, cloud):
+        response = requests.get(
+            self.URL.format(temp, g, r_planet, r_star, metallicity, c_o, haze, cloud)
+        )
         try:
-            soup = BeautifulSoup(response.content, 'html.parser')
+            soup = BeautifulSoup(response.content, "html.parser")
             input_tag = soup.find_all("input")
             data_file = None
             for i in input_tag:
-                if i.get("name")=="data_file":
-                    data_file=i.get("value")
+                if i.get("name") == "data_file":
+                    data_file = i.get("value")
                     break
             return data_file
         except:
--- FileHelper.py
+++ FileHelper.py
@@ -1,13 +1,14 @@
 import os
 
+
 class FileHelper:
-    def removeAll(self,folder_path):
+    def removeAll(self, folder_path):
         files = os.listdir(folder_path)
         for file in files:
             file_path = os.path.join(folder_path, file)
             if os.path.isfile(file_path):
                 os.remove(file_path)
 
-    def insertOne(self,path,file_name,file):
-        with open('{}/{}'.format(path,file_name), 'w') as f:
+    def insertOne(self, path, file_name, file):
+        with open("{}/{}".format(path, file_name), "w") as f:
             f.write(file)
